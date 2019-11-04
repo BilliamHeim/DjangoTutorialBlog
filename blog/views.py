8dada5ce1c760ee5b2a8e2ae0d70904de1ce27f3
LiquidBilliam <will@buildlabs.io>
2019-11-04 17:46:30 -0600

    Part 3 completed

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'BillyTwoShoes',
+        'title': 'Primero Post',
+        'content': 'Hello world! (From first dictionary entry)',
+        'date_posted': '11.4.2019'
+    },
+    {
+        'author': 'BillyTwoShoes',
+        'title': 'Post Deux',
+        'content': 'Hello world! (From second dictionary entry)',
+        'date_posted': '11.5.2019'
+    }
+]
+
+# Create your views here.
+def home(request):
+    context ={
+        'posts':  posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':  'About'})
